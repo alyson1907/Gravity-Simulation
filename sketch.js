c3d4f32f7125736ba433e66c3f82fe6e0a921c3d
Alyson Matheus <alyson1907@gmail.com>
2020-05-29 20:27:30 -0300

    initial commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// =============================================================================================
+// ===================================== MAIN PROGRAM ==========================================
+// =============================================================================================
+
+// HTML elements
+let canvas
+const canvasWidth = 1080
+const canvasHeight = 700
+
+function setup () {
+  createCanvas(canvasWidth, canvasHeight)
+  background('#ddd')
+  canvas = document.getElementsByTagName('canvas')[0]
+
+}
+
+function draw () {
+
+
+}
