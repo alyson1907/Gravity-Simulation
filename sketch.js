33594a3c6bed91ef56b7b6447f29c0947529d634
Alyson Matheus <alyson1907@gmail.com>
2020-06-04 11:47:14 -0300

    ^simple updates @sketch.js

--- sketch.js
+++ sketch.js
@@ -3,18 +3,19 @@ const G = 15
 const blackHoles = []
 const particles = []
 
-const numberOfParticles = 5
+const numberOfParticles = 3
 
 function setup () {
   createCanvas(windowWidth, windowHeight)
   background('#ddd')
-  blackHoles.push(new BlackHole(700, 350, 20))
+  blackHoles.push(new BlackHole(1000, 350, 20))
   for (let i = 0; i < numberOfParticles; i++) {
     particles.push(new Particle(random(windowWidth), random(windowHeight), 4))
   }
 }
 
 function draw () {
+  // Uncomment `clear()` below to remove trailing for each particle
   // clear()
   blackHoles.forEach(bh => {
     bh.show()
